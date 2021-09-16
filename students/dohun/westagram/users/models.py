6e9a23db8bfe36f501b241e71b87e21d8ccf4498
Your Name <kimdh1004yo@gmail.com>
2021-09-16 21:56:32 +0900

    모델링

--- students/dohun/westagram/users/models.py
+++ students/dohun/westagram/users/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class User(models.Model):
     name             = models.CharField(max_length=100)
-    user_name        = models.CharField(max_length = 100)
-    email            = models.EmailField(max_length=100)
-    password         = models.CharField(max_length=45)
+    email            = models.CharField(max_length=100)
+    password         = models.CharField(max_length=45)  
     phone_number     = models.IntegerField()
+    etc_info         = models.CharField(max_length = 500, null=True)
+
 
     class Meta:
         db_table = 'users'
