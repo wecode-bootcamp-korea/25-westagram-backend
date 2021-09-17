16d71b2bc80dea197401d91e243a1bf22f414c81
Your Name <kimdh1004yo@gmail.com>
2021-09-17 10:58:13 +0900

    수정

--- students/dohun/westagram/users/models.py
+++ students/dohun/westagram/users/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 class User(models.Model):
     name             = models.CharField(max_length=100)
-    email            = models.CharField(max_length=100)
-    password         = models.CharField(max_length=45)  
+    email            = models.CharField(max_length=100, unique=True)
+    password         = models.CharField(max_length=200)  
     phone_number     = models.IntegerField()
     etc_info         = models.CharField(max_length = 500, null=True)
-
+    created_at       = models.DateTimeField(auto_now_add=True)
+    updated_at       = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
+
