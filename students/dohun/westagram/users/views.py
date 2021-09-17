190aece4e830f33e8a8db2f3326e9c6b694dcedd
Your Name <kimdh1004yo@gmail.com>
2021-09-17 16:14:42 +0900

    회원가입 views

--- students/dohun/westagram/users/views.py
+++ students/dohun/westagram/users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+from .models import User
+
+class UserView(View):
+    def post(self, request):
+
+        data               = json.loads(request.body)
+        password_check     = data['password']
+        email_check        = data['email']
+
+        try:
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email_check):
+                return JsonResponse({'MESSAGE' : 'RETYPE_EMAIL'}, status=400)
+
+            if not re.match('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}', password_check):
+                return JsonResponse({'MESSAGE' : 'RETYPE_PASSWORD'}, status=400)
+
+            if User.objects.filter(email=email_check).exists():
+                return JsonResponse({'MESSAGE' : 'EXISTING_EMAIL'}, status=400)
+
+            User.objects.create(
+                name             = data['name'],
+                email          = email_check,
+                password         = data['password'],
+                phone_number    = data['phone_number'],
+                etc_info        = data['etc_info']
+            )
+
+            return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=201)
+        
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+        
