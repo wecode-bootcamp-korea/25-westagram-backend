513034a10a20793024194744bd2f7edaca8e1270
thisisempty <thisisemptyyy@gmail.com>
2021-09-17 11:17:25 +0900

    ADD column [created_at, updated_at], modify column [password] max_length 45 -> 200

--- students/dabeen/westagram/users/models.py
+++ students/dabeen/westagram/users/models.py
@@ -5,10 +5,12 @@ class User(models.Model):
     # 이름, 이메일, 비밀번호, 연락처, 휴대폰, 그 외 개인정보
     name = models.CharField(max_length=45)
     email = models.CharField(max_length=45, unique=True)
-    password = models.CharField(max_length=45)
+    password = models.CharField(max_length=200)
     phone = models.IntegerField()
     web_site = models.CharField(max_length=100, null=True)
     information = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
