6cd997538f484b7dd95bd664c05d89a107829657
Your Name <kimdh1004yo@gmail.com>
2021-09-19 21:41:05 +0900

    로그인 기능 구현

--- students/dohun/westagram/users/views.py
+++ students/dohun/westagram/users/views.py
@@ -34,3 +34,25 @@ class UserView(View):
         
         except KeyError:
             return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+
+        data = json.loads(request.body)
+        email = data['email']
+        password = data['password']
+
+        try:
+            if not User.objects.filter(email = email).exists():
+                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status = 401)
+
+            user = User.objects.get(email = email)
+
+            if user.password != password:
+                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status = 401)
+            
+            return JsonResponse({'MESSAGE': 'SUCCESS'}, status = 200)
+
+        
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'Key_Error'}, status = 401)
