882c9b308a0f88c8f839cd0b46fb59f07528df39
KIM <nzlk112@gmail.com>
2021-09-22 17:41:48 +0900

    user register complete

--- students/minchan/westagram/users/views.py
+++ students/minchan/westagram/users/views.py
@@ -3,23 +3,31 @@ import re
 from django.http.response   import JsonResponse
 from django.views           import View
 from users.models           import Users
+from django.http            import HttpResponse
 
 # Create your views here.
 class user_register(View) :
-    def vaild_email(email) :
+    def valid_email(self,email) :
         email_regular       = re.compile("[@][a-zA-Z]*[.]")
         if email_regular.search(email) == None :
             return False
         return True
+
+
+    def check_email_duplicate(self,email) :
+        if len(Users.objects.filter(email=email)) == 0 :
+            return True
+        return False
     
-    def valid_pw(pw) :
+
+    def valid_pw(self,pw) :
         # 검사항목 - 0: 8자리 이상, 1:문자포함, 2:숫자포함, #:특수문자포함
         test_case           = [0,0,0,0]
         alphabet_regular    = re.compile("[a-zA-Z]")
         number_regular      = re.compile("[\d]")
-        symbol_regular      = re.complie("[`~!@#$%^&*(),<.>/?]") #더 많은 특수문자의 허용은 [^0-9a-zA-Z]로 대체 가능.
+        symbol_regular      = re.compile("[`~!@#$%^&*(),<.>/?]") #더 많은 특수문자의 허용은 [^0-9a-zA-Z]로 대체 가능.
         #0 길이검사.
-        if len(pw) > 8 : 
+        if len(pw) >= 8 : 
             test_case[0] = 1
 
         #1. 알파벳 포함 검사
@@ -40,8 +48,8 @@ class user_register(View) :
                 test_case[3] = 1
                 break
 
-        return test_case
-        
+        return test_case  
+
 
     def post(self,request) :
         data                = json.loads(request.body)
@@ -60,12 +68,16 @@ class user_register(View) :
         elif not self.valid_email(data_email) :
             return JsonResponse({"MESSAGE" : "이메일 양식이 틀렸습니다."}, status=400)
         
-            #3. 패스워드 유효성 검사
+        #3. 패스워드 유효성 검사
         elif  password_check != [1,1,1,1] :
             for i in range(4) :
                 if password_check[i] != 1 :
                     wrong_pw_message += wrong_pw_list[i]
             return JsonResponse({"MESSAGE" : wrong_pw_message}, status=400)
+        
+        #4. 이메울 중복 검사
+        if not self.check_email_duplicate(data_email) :
+            return JsonResponse({"MESSAGE" : "Email duplicate"}, status=400)
         
         Users.objects.create(
             name            = data["name"],
@@ -75,5 +87,7 @@ class user_register(View) :
             profile_etc     = data["profile_etc"],
             )
 
-
         return JsonResponse({"MESSAGE" : "CREATED"}, status=201)
+
+    def get(self,request) :
+        return HttpResponse("HI")
--- students/minchan/westagram/westagram/urls.py
+++ students/minchan/westagram/westagram/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from users.views import user_register
 
 urlpatterns = [
-    path('/user_register', user_register.as_view(), name='user_register'),
+    path('minchan/user_register', user_register.as_view(), name='user_register'),
 ]
