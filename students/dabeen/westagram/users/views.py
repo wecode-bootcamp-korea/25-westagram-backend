a3ff81294e61baf3ad4b4d92e3afd0b1069d6bcd
thisisempty <thisisemptyyy@gmail.com>
2021-09-23 19:46:40 +0900

    [Modify] Change the phone column from an IntegerField to a CharField.

--- students/dabeen/westagram/users/views.py
+++ students/dabeen/westagram/users/views.py
@@ -1,15 +1,15 @@
-from users.models import User
-from django.shortcuts import render
-
 import json
 import re
+
+from users.models import User
 from django.http import JsonResponse
 from django.views import View
+
+# Create your views here.
 
 
 class SignUpView(View):
     def post(self, request):
-
         try:
             data = json.loads(request.body)
 
@@ -19,13 +19,13 @@ class SignUpView(View):
             if User.objects.filter(email=email).exists():
                 return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=400)
 
-            regex_email = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-            regex_password = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}'
+            REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            REGEX_PASSWORD = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}'
 
-            if not re.match(regex_email, email):
+            if not re.match(REGEX_EMAIL, email):
                 return JsonResponse({'message': 'INVAILD_EMAIL'})
 
-            if not re.match(regex_password, password):
+            if not re.match(REGEX_PASSWORD, password):
                 return JsonResponse({'message': 'INVAILD_PASSWORD'})
 
             User.objects.create(
@@ -36,5 +36,55 @@ class SignUpView(View):
             )
 
             return JsonResponse({'message': 'SUCCESS'}, status=201)
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email = data['email']
+            password = data['password']
+
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=400)
+
+            REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            REGEX_PASSWORD = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}'
+
+            if not re.match(REGEX_EMAIL, email):
+                return JsonResponse({'message': 'INVAILD_EMAIL'})
+
+            if not re.match(REGEX_PASSWORD, password):
+                return JsonResponse({'message': 'INVAILD_PASSWORD'})
+
+            User.objects.create(
+                name=data["name"],
+                email=email,
+                password=password,
+                phone=data["phone"],
+            )
+
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
+
+
+class SignInView(View):
+    # Use User's email and password
+    def get(self, request):
+        data = json.loads(request.body)
+        try:
+
+            if User.objects.filter(email=data['email']).exists():
+                user = User.objects.get(email=data['email'])
+
+                if user.password == data['password']:
+                    return JsonResponse({"message": "SUCCESS"}, status=200)
+                # If user enter the wrong password return 401 ERROR
+                return JsonResponse({'message': 'INVAILD_USERS'}, status=401)
+
+            # If user enter the wrong email return 401 ERROR
+            return JsonResponse({'message': "INVAILD_USERS"}, status=401)
+
+        except:
+            # If user's email or password is not delivered return 400 ERROR
+            return JsonResponse({'message': "KEY_ERROR"}, status=400)
