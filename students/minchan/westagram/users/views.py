fc8cc8b1773e96ec89817b6a16632d90acb9af02
KIM <nzlk112@gmail.com>
2021-09-23 11:19:35 +0900

    회원가입 시 반환항목 400 -> 401로 수정, 로그인 비밀번호 확인함수 압축

--- students/minchan/westagram/users/views.py
+++ students/minchan/westagram/users/views.py
@@ -8,7 +8,7 @@ from django.http            import HttpResponse
 # Create your views here.
 class user_register(View) :
     def valid_email(self,email) :
-        email_regular       = re.compile("[@][a-zA-Z]*[.]")
+        email_regular = re.compile("[@][a-zA-Z]*[.]")
         if email_regular.search(email) == None :
             return False
         return True
@@ -62,22 +62,22 @@ class user_register(View) :
         
         #1. 이메일, 패스워드 있는지 확인하기.
         if data_email == None or data_pw == None :
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=401)
 
         #2. 이메일 유효성 검사
         if not self.valid_email(data_email) :
-            return JsonResponse({"MESSAGE" : "이메일 양식이 틀렸습니다."}, status=400)
+            return JsonResponse({"MESSAGE" : "이메일 양식이 틀렸습니다."}, status=401)
         
         #3. 패스워드 유효성 검사
         if  password_check != [1,1,1,1] :
             for i in range(4) :
                 if password_check[i] != 1 :
                     wrong_pw_message += wrong_pw_list[i]
-            return JsonResponse({"MESSAGE" : wrong_pw_message}, status=400)
+            return JsonResponse({"MESSAGE" : wrong_pw_message}, status=401)
         
         #4. 이메울 중복 검사
         if self.is_email_duplicate(data_email) :
-            return JsonResponse({"MESSAGE" : "Email duplicate"}, status=400)
+            return JsonResponse({"MESSAGE" : "Email duplicate"}, status=401)
         
         Users.objects.create(
             name            = data["name"],
@@ -96,14 +96,13 @@ class user_login(View) :
     def check_user_exist(self,email) :
         # 이메일이 존재하면 True를 반환.
         if Users.objects.filter(email=email) :
-            return True 
+            return True
         return False
     
     def check_pw_match(self,email,pw) :
         #받은 이메일,패스워드가 일치하면 True반환.
-        if Users.objects.filter(email=email)[0].password == pw :
-            return True
-        return False
+        return Users.objects.filter(email=email)[0].password == pw
+
 
     def post(self,request) :
         data        = json.loads(request.body)
@@ -114,12 +113,9 @@ class user_login(View) :
         if (not data_email) and (not data_pw) :
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         
-        #2. 계정이 있는지 여부
-        if not self.check_user_exist(data_email) :
-            return JsonResponse({"message": "INVALID_USER"}, status=401)
-        
-        #3. 패스워드가 맞는지 여부        
-        if not self.check_pw_match(data_email,data_pw) :
-            return JsonResponse({"message": "INVALID_USER"}, status=401)
+        #2. 계정이 있는지 여부, 3. 패스워드가 맞는지 여부
+        if (not self.check_user_exist(data_email)) and (not self.check_pw_match(data_email,data_pw)):
+            return JsonResponse({"message": "INVALID_USER"}, status=401)    
 
         return JsonResponse({"message": "SUCCESS"}, status=200)
+
