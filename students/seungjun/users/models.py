32d984c62b7178634a87de3177630b3990529b09
palza4dev <moonsj6940@gmail.com>
2021-09-26 00:38:55 +0900

    <Modify> users 앱 models의 field option 수정 - gender와 date_birth 필드는 not required 하므로 필드 옵션 수정 - 문자열 타입은 null이 아닌 blank로 표현하려함. - DATE 타입은 null 가능

--- students/seungjun/users/models.py
+++ students/seungjun/users/models.py
@@ -5,8 +5,8 @@ class User (models.Model):
     email         = models.EmailField(max_length=100, unique=True)
     password      = models.CharField(max_length=200)
     phone_number  = models.CharField(max_length=15, help_text="000-0000-0000")
-    gender        = models.CharField(max_length=10, null=True)
-    date_birth    = models.DateField(null=True)
+    gender        = models.CharField(max_length=10, blank=True)
+    date_birth    = models.DateField(null=True, blank=True)
     updated_at    = models.DateTimeField(auto_now=True)
     created_at    = models.DateTimeField(auto_now_add=True)
 
