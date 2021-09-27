2cd8c4ee3fd53130c61c54f5f03aa1b65164be9d
jh <bndgc@naver.com>
2021-09-27 17:01:50 +0900

    [Add] SignUpView로 회원가입 구현 - 이메일, 비밀번호 입력되었는지 검사 - 이메일 중복 검사 - 이메일, 비밀번호 조건 충족 여부 검사

--- students/juhyeong/users/views.py
+++ students/juhyeong/users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import json
+import re
+import bcrypt
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from users.models import User
+from my_settings import SECRET_KEY
+
+class SignUpsView(View):
+    def post(self, request):
+        data             = json.loads(request.body)
+        email_checker    = re.compile('^[\w]+@[\w.\-]+\.[A-Za-z]{2,3}$')
+        password_checker = re.compile('^(?=.*[A-Za-z])(?=*\d)(?=.*[~!@#$^&*()+|=])[(A-Za-z\d~!@#$%^&*+|=)]{8,}$')
+        user             = User.objects
+
+        if 'email' or 'data' not in data:
+            return JsonResponse({"message": "KEY_ERROR"}, status =400)
+
+        if user.filter().exists():
+            return JsonResponse({"message": "KEY_ERROR"}, status =401)
+
+        elif not email_checker.match(data['email']) != None:
+            return JsonResponse({"message": "이메일 형식이 잘못 되었습니다"}, status =401)
+        
+        elif len() < 8 or not password_checker.match(data['password']) != None:
+            return JsonResponse({"message": "비밀번호 조건을 충족되지 않습니다"}, status =401)
+
+        hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+
+        user.create(
+            name         = data['name'],
+            email        = data['email'],
+            password     = hashed_password.decode('utf-8'),
+            phone_number = data['phone_number'],
+            profile      = data['profile']
+        )
+        return JsonResponse({'CREATED':'SUCCESS'}, status = 201)
