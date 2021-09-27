88a28851310eb5d4c942edf6deb1206672e0eedc
jh <bndgc@naver.com>
2021-09-27 18:11:23 +0900

    [Modified] Signupsview post error 수정 - re.error: password_checker (?=.*)에서 .누락 (두 시간 순삭)

--- students/juhyeong/users/views.py
+++ students/juhyeong/users/views.py
@@ -10,19 +10,19 @@ class SignUpsView(View):
     def post(self, request):
         data             = json.loads(request.body)
         email_checker    = re.compile('^[\w]+@[\w.\-]+\.[A-Za-z]{2,3}$')
-        password_checker = re.compile('^(?=.*[A-Za-z])(?=*\d)(?=.*[~!@#$^&*()+|=])[(A-Za-z\d~!@#$%^&*+|=)]{8,}$')
-        user             = User.objects
+        password_checker = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,}$')
+        user             = User.objects.all()
 
-        if 'email' or 'data' not in data:
+        if 'email' not in data or 'password' not in data:
             return JsonResponse({"message": "KEY_ERROR"}, status =400)
 
-        if user.filter().exists():
-            return JsonResponse({"message": "KEY_ERROR"}, status =401)
+        if user.filter(email=data['email']).exists():
+            return JsonResponse({"message": "이메일이 이미 등록되었습니다"}, status =401)
 
         elif not email_checker.match(data['email']) != None:
             return JsonResponse({"message": "이메일 형식이 잘못 되었습니다"}, status =401)
         
-        elif len() < 8 or not password_checker.match(data['password']) != None:
+        elif len(data['password']) < 8 or not password_checker.match(data['password']) != None:
             return JsonResponse({"message": "비밀번호 조건을 충족되지 않습니다"}, status =401)
 
         user.create(
