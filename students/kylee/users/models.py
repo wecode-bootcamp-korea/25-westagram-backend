761617de915223fa3669ee2cb2ee7142c19ad140
kylee <leeky940926@gmail.com>
2021-09-27 16:18:28 +0900

    수정사항 반영했습니다

--- students/kylee/users/models.py
+++ students/kylee/users/models.py
@@ -1,12 +1,13 @@
 from django.db            import models
 
-# Create your models here.
 class Users(models.Model) :
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, primary_key=True)
-    password = models.CharField(max_length=500)
-    tel = models.CharField(max_length=15)
-    birthday = models.DateField()
+    name       = models.CharField(max_length=50)
+    email      = models.EmailField(max_length=50, primary_key=True, unique=True)
+    password   = models.CharField(max_length=500)
+    telephone  = models.CharField(max_length=15)
+    birthday   = models.DateField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta :
         db_table = 'users'
