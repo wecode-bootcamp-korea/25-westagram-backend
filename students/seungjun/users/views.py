177a1451e595ee5f49d0aa85e9ce7aa2043bec9e
palza4dev <moonsj6940@gmail.com>
2021-09-27 18:36:58 +0900

    <Style> 더 나은 가독성을 위한 정규 표현식 코드 수정 - 이메일, 비밀번호, 생년월일 validation 코드 포맷팅 수정 - 불필요한 변수 할당 제거 - 코드의 양을 줄이고 가독성을 높이기 위함

--- students/seungjun/users/views.py
+++ students/seungjun/users/views.py
@@ -22,19 +22,13 @@ class SignUpView(View):
             if User.objects.filter(email=email).exists():
                 return JsonResponse({"message":"Email already exists"}, status=400)
 
-            regex_email = re.match(
-                '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)
-            if not regex_email:
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
                 return JsonResponse({'message':'Email format is not valid'}, status=400)
 
-            regex_password = re.match(
-                '^(?=.*[A-Za-z])(?=.*\d)(?=.*[?!@#$%*&])[A-Za-z\d?!@#$%*&]{8,}$', password)
-            if not regex_password:
+            if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[?!@#$%*&])[A-Za-z\d?!@#$%*&]{8,}$', password):
                 return JsonResponse({'message':'Password format is not valid'}, status=400)
 
-            regex_date_birth = re.match(
-                '^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$', date_birth)
-            if not regex_date_birth: 
+            if not re.match('^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$', date_birth): 
                 return JsonResponse({'message':'Date format must be in YYYY-MM-DD'}, status=400)
 
             User.objects.create(
