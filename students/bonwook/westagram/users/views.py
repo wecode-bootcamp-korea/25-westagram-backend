232157cf1b30b5a310c2daacc4247cfc8bd700b3
qhsdnr0 <qhsdnr0@gmail.com>
2021-09-27 16:42:45 +0900

    First Modify: users modeling

--- students/bonwook/westagram/users/views.py
+++ students/bonwook/westagram/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.http.response import HttpResponse
+from django.views import View
+
+from .models import User
+
+class UserView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        user = User.objects.create(
+            name            = data['name'],
+            email           = data['email'],
+            password        = data['password'],
+            phone_number    = data['phone_number'],
+            date_of_birth   = data['date_of_birth'], 
+        )
+
+        return HttpResponse({'MESSAGE' : 'CREATED'}, status = 201)
