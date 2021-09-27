c06fb0221aaac0e4f01892763ae128257efd2611
Your Name <kimdh1004yo@gmail.com>
2021-09-27 13:24:54 +0900

    view.py 수정

--- students/dohun/westagram/users/views.py
+++ students/dohun/westagram/users/views.py
@@ -1,33 +1,33 @@
 import json
 import re
 
-from django.http import JsonResponse
+from django.http  import JsonResponse
 from django.views import View
-from .models import User
+from .models      import User
 
 class UserView(View):
     def post(self, request):
 
-        data               = json.loads(request.body)
-        password_check     = data['password']
-        email_check        = data['email']
+        data     = json.loads(request.body)
+        password = data['password']
+        email    = data['email']
 
         try:
-            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email_check):
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
                 return JsonResponse({'MESSAGE' : 'RETYPE_EMAIL'}, status=400)
 
-            if not re.match('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}', password_check):
-                return JsonResponse({'MESSAGE' : 'RETYPE_PASSWORD'}, status=400)
+            if not re.match('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}', password):
+                return JsonResponse({'MESSAGE' : 'INVALID_FORMAT'}, status=400)
 
-            if User.objects.filter(email=email_check).exists():
+            if User.objects.filter(email=email).exists():
                 return JsonResponse({'MESSAGE' : 'EXISTING_EMAIL'}, status=400)
 
             User.objects.create(
-                name           = data['name'],
-                email          = email_check,
-                password       = password_check,
-                phone_number   = data['phone_number'],
-                etc_info       = data['etc_info']
+                name         = data['name'],
+                email        = email,
+                password     = password,
+                phone_number = data['phone_number'],
+                etc_info     = data['etc_info']
             )
 
             return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=201)
@@ -38,8 +38,8 @@ class UserView(View):
 class LoginView(View):
     def post(self, request):
 
-        data = json.loads(request.body)
-        email = data['email']
+        data     = json.loads(request.body)
+        email    = data['email']
         password = data['password']
 
         try:
@@ -50,7 +50,7 @@ class LoginView(View):
 
             if user.password != password:
                 return JsonResponse({'MESSAGE': 'INVALID_USER'}, status = 401)
-            
+        
             return JsonResponse({'MESSAGE': 'SUCCESS'}, status = 200)
 
         
