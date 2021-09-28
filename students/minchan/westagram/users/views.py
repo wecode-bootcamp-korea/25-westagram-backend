7a7e0b129883eb2b632baa98153439c555279b7b
KIM <nzlk112@gmail.com>
2021-09-28 13:11:27 +0900

    bcrypt 반영

--- students/minchan/westagram/users/views.py
+++ students/minchan/westagram/users/views.py
@@ -1,5 +1,6 @@
 import json
 import re
+import bcrypt
 from django.http.response   import JsonResponse
 from django.views           import View
 from users.models           import Users
@@ -22,7 +23,7 @@ class UserRegister(View) :
         data            = json.loads(request.body)
         email           = data["email"]
         password        = data["password"]
-
+        password_bcrypt = bcrypt.hashpw(bytes(password,"utf-8"),bcrypt.gensalt())
 
         #이메일, 패스워드의 유효성 및 중복에 대해 검사.        
         if not (email or password) :
@@ -40,7 +41,7 @@ class UserRegister(View) :
         Users.objects.create(
             name            = data["name"],
             email           = email,
-            password        = password,
+            password        = password_bcrypt,
             phone_number    = data["phone_number"],
             profile_etc     = data["profile_etc"],
             )
@@ -50,7 +51,10 @@ class UserRegister(View) :
 class UserLogin(View) :
     def is_user_exist(self,email) :
         return Users.objects.filter(email=email).exists()
-
+    
+    def is_pw_match(self,email,password) :
+        return bcrypt.checkpw(password.encode('utf-8'), Users.objects.filter(email=email)[0].password[2:-1].encode('utf-8'))
+    
     def post(self,request) :
         data        = json.loads(request.body)
         email       = data["email"]
@@ -59,7 +63,7 @@ class UserLogin(View) :
         if not (email and password) :
             return JsonResponse({"message": "KEY_ERROR"}, status=401)
         
-        if not (self.is_user_exist(email)):
+        if not (self.is_user_exist(email) and self.is_pw_match(email,password)):
             return JsonResponse({"message": "INVALID_USER"}, status=401)
 
         return JsonResponse({"message": "SUCCESS"}, status=200)
