c4900e125b63cbd743360b008ba78cbdf2e8d4e9
박현우 <pang@baghyeon-uui-MacBookAir.local>
2021-09-28 15:49:58 +0900

    Add: login

--- students/hyeon_woo/westagram/users/views.py
+++ students/hyeon_woo/westagram/users/views.py
@@ -7,28 +7,20 @@ from django.views     import View
 from users.models     import User
 
 REGEX_EMAIL    = r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-REGEX_PASSWORD = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]"
+REGEX_PASSWORD = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$"
 
 class SignUpView(View):
 
     def post(self,request):
         data = json.loads(request.body)
         try:
-
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({"message": "Please use a different email"}, status=400)
-            if (data["password"] == ""):
-                return JsonResponse({"message": "Please enter a password"}, status=400)
-            if (data["email"] == ""):
-                return JsonResponse({"message": "Please enter a email"}, status=400)
             
             if not re.match(REGEX_EMAIL, data["email"]):
                 return JsonResponse({"message": "Email_Error : Need @ and ."}, status=400)
             if not re.match(REGEX_PASSWORD, data["password"]):
-                return JsonResponse({"message": "Password_Error : Need LETTER,NUMBER,SPECIAL_SYMBOLS"}, status=400)
-    
-            if len(data["password"]) < 8 :
-                return JsonResponse({"message": "Password must be at least 8 characters in length"}, status=400)
+                return JsonResponse({"message": "Password_Error : Need 8LETTER,NUMBER,SPECIAL_SYMBOLS"}, status=400)
 
             User.objects.create(
                 name          = data['name'],
@@ -47,7 +39,6 @@ class LoginView(View):
     def post(self,request):
         data = json.loads(request.body)
         try :
-
             if not User.objects.filter(email=data['email']).exists():
                 return JsonResponse({"message": "INVALID_USER"}, status=401)
 
