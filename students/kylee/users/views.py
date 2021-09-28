f1f9bfa64e4a4d40e8962c36c8448b80fc7e820d
kylee <leeky940926@gmail.com>
2021-09-28 17:47:08 +0900

    1st login finished

--- students/kylee/users/views.py
+++ students/kylee/users/views.py
@@ -3,7 +3,7 @@ import re
 
 from django.views import View
 from django.http  import JsonResponse
-from users.models import Users
+from users.models import User
 
 class SignupView(View) :
     def post(self, request) :
@@ -24,14 +24,14 @@ class SignupView(View) :
                 if not re.match('^[\w+-\_.]+@[\w]+\.[\w]+$', email) :
                     return JsonResponse({'message':'이메일은 @ 와 . 이 형식에 맞게 순서대로 들어가야 합니다.'}, status=400)
 
-                if Users.objects.filter(email=email).exists() :
+                if User.objects.filter(email=email).exists() :
                     return JsonResponse({'message':'기존재 이메일입니다.'}, status=400)
 
             if password :
                 if not re.match('^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])[a-zA-Z0-9!@#$%^&*()_+=-]{8,}$', password) :
                     return JsonResponse({'message':'비밀번호에는 숫자/문자/특수문자가 1개씩 들어가야 합니다.'}, status=400)
 
-            Users.objects.create(
+            User.objects.create(
                 email     = email,
                 password  = password,
                 name      = data['name'],
@@ -41,5 +41,32 @@ class SignupView(View) :
 
             return JsonResponse({'mesage':'SUCCESS'},status=201)
         
+        except Exception as msg :
+            return JsonResponse({'message':msg}, status=400)
+
+class LoginView(View) :
+    def post(self, request) :
+        try :
+            data = json.loads(request.body)
+
+            email    = data.get('email',    None)
+            password = data.get('password', None)
+
+            if not email :
+                return JsonResponse({'message':'KEY_ERROR BY EMAIL'}, status=400)
+            
+            if not password :
+                return JsonResponse({'message':'KEY_ERROR BY PASSWORD'}, status=400)
+            
+            if email :
+                if not User.objects.filter(email=email).exists() :
+                    return JsonResponse({'message':'INVALID_USER BY EMAIL'}, status=401)
+                
+                else :
+                    if not User.objects.filter(password=password).exists() :
+                        return JsonResponse({'message':'INVALID USER BY PASSWORD'}, status=401)
+            
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
         except Exception as msg :
             return JsonResponse({'message':msg}, status=400)
