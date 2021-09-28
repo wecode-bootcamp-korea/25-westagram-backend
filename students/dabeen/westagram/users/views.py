f406718060349d8e7f8c257f0c949b1e9a450410
thisisempty <thisisemptyyy@gmail.com>
2021-09-28 13:54:05 +0900

    # This is a combination of 3 commits. # This is the 1st commit message:
    modify phone columm max_lengt 20 to 100
    
    # This is the commit message #2:
    
    [ADD]
    users/urls.py : add SignInView path
    
    [Modify]
    users/views.py : delete duplicated part
    
    # This is the commit message #3:
    
    modify duplicated part

--- students/dabeen/westagram/users/views.py
+++ students/dabeen/westagram/users/views.py
@@ -5,38 +5,6 @@ from users.models import User
 from django.http import JsonResponse
 from django.views import View
 
-# Create your views here.
-
-
-class SignUpView(View):
-    def post(self, request):
-        try:
-            data = json.loads(request.body)
-
-            email = data['email']
-            password = data['password']
-
-            if User.objects.filter(email=email).exists():
-                return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=400)
-
-            REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-            REGEX_PASSWORD = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}'
-
-            if not re.match(REGEX_EMAIL, email):
-                return JsonResponse({'message': 'INVAILD_EMAIL'})
-
-            if not re.match(REGEX_PASSWORD, password):
-                return JsonResponse({'message': 'INVAILD_PASSWORD'})
-
-            User.objects.create(
-                name=data["name"],
-                email=email,
-                password=password,
-                phone=data["phone"],
-            )
-
-            return JsonResponse({'message': 'SUCCESS'}, status=201)
-
 
 class SignUpView(View):
     def post(self, request):
@@ -71,20 +39,18 @@ class SignUpView(View):
 class SignInView(View):
     # Use User's email and password
     def get(self, request):
-        data = json.loads(request.body)
         try:
+            data = json.loads(request.body)
 
             if User.objects.filter(email=data['email']).exists():
-                user = User.objects.get(email=data['email'])
+                return JsonResponse({'message': 'UKNOWN_USER'}, status=400)
 
-                if user.password == data['password']:
-                    return JsonResponse({"message": "SUCCESS"}, status=200)
-                # If user enter the wrong password return 401 ERROR
+            user = User.objects.get(email=data['email'])
+
+            if user.password != data['password']:
                 return JsonResponse({'message': 'INVAILD_USERS'}, status=401)
 
-            # If user enter the wrong email return 401 ERROR
-            return JsonResponse({'message': "INVAILD_USERS"}, status=401)
+            return JsonResponse({"message": "SUCCESS"}, status=200)
 
-        except:
-            # If user's email or password is not delivered return 400 ERROR
+        except KeyError:
             return JsonResponse({'message': "KEY_ERROR"}, status=400)
