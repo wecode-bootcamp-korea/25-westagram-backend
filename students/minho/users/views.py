3429bb86382a3a2be7bd8b09bf533ffa1e404358
minho <maxkmh712@gmail.com>
2021-09-28 14:44:15 +0900

    [ADD] user app 로그인 기능 추가
    - user/views.py SignInView class 추가
    - user/urls.py SignInView import 추가, url path 추가

--- students/minho/users/views.py
+++ students/minho/users/views.py
@@ -41,3 +41,22 @@ class SignUpView(View):
 			
 		except KeyError:
 			return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400)
+
+class SignInView(View):
+	def get(self, request):
+		try :
+			data 	 = json.loads(request.body)
+
+			email    = data['email']
+			password = data['password']
+
+			if not User.objects.filter(email = email).exists():
+					return JsonResponse({'MESSAGE' : 'INVALID_USER'}, status = 401)
+			
+			if not User.objects.get(email = email).password == password:
+					return JsonResponse({'MESSAGE':'INVALIED_USER'}, status = 401)
+			
+			return JsonResponse({'MESSGAE' : 'SUCCESS'}, status = 200)
+
+		except KeyError :
+			return JsonResponse({'MESSAGE' : "KEY_ERROR"}, status = 400)
