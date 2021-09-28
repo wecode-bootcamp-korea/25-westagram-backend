de23b9f583552e044bf6ccc6162053fc79c9f661
박현우 <pang@baghyeon-uui-MacBookAir.local>
2021-09-28 15:46:19 +0900

    Add: password_encrypt

--- students/hyeon_woo/westagram/users/views.py
+++ students/hyeon_woo/westagram/users/views.py
@@ -10,7 +10,7 @@ from users.models    import User
 from my_settings     import SECRET_KEY, ALGORITHM
 
 REGEX_EMAIL    = r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-REGEX_PASSWORD = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]"
+REGEX_PASSWORD = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$"
 
 class SignUpView(View):
 
@@ -21,28 +21,20 @@ class SignUpView(View):
         decode_password = encode_password.decode('utf-8')
 
         try:
-
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({"message": "Please use a different email"}, status=400)
-            if (data["password"] == ""):
-                return JsonResponse({"message": "Please enter a password"}, status=400)
-            if (data["email"] == ""):
-                return JsonResponse({"message": "Please enter a email"}, status=400)
             
             if not re.match(REGEX_EMAIL, data["email"]):
                 return JsonResponse({"message": "Email_Error : Need @ and ."}, status=400)
             if not re.match(REGEX_PASSWORD, data["password"]):
-                return JsonResponse({"message": "Password_Error : Need LETTER,NUMBER,SPECIAL_SYMBOLS"}, status=400)
-    
-            if len(data["password"]) < 8 :
-                return JsonResponse({"message": "Password must be at least 8 characters in length"}, status=400)
+                return JsonResponse({"message": "Password_Error : Need 8LETTER,NUMBER,SPECIAL_SYMBOLS"}, status=400)
 
             User.objects.create(
                 name          = data['name'],
                 email         = data['email'],
                 password      = decode_password,
                 phone_number  = data['phone_number'],
-                information   = data['imformation'],
+                information   = data['information'],
             )
 
             return JsonResponse({"message": "SUCCESS"}, status=201)
@@ -55,7 +47,6 @@ class LoginView(View):
         data  = json.loads(request.body)
 
         try :
-
             if not User.objects.filter(email=data['email']).exists():
                 return JsonResponse({"message": "INVALID_USER"}, status=401)
 
