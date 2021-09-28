0583fc7844ba369f85bb4d648c3ecbd7d13b1b05
jh <bndgc@naver.com>
2021-09-28 15:57:49 +0900

    [Modified] models.py - profile null=True 추가

--- students/juhyeong/users/models.py
+++ students/juhyeong/users/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     email        = models.CharField(max_length=200, unique=True)     
     password     = models.CharField(max_length=200)    
     phone_number = models.CharField(max_length=11)    
-    profile      = models.CharField(max_length=255)
+    profile      = models.CharField(max_length=255, null=True)
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True) 
 
