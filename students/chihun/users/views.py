a89179a4ea722661b58c0e4f78eefb4c3f927ef8
chihunmanse <chihunmanse@gmail.com>
2021-09-28 15:08:38 +0900

    [MODIFY] westagram users signin

--- students/chihun/users/views.py
+++ students/chihun/users/views.py
@@ -1,4 +1,5 @@
 import json
+
 from django.http            import JsonResponse
 from django.views           import View
 from django.core.exceptions import ValidationError
@@ -50,15 +51,18 @@ class SignIn(View):
             password = data['password']
 
             if not User.objects.filter(email = email).exists() :
-                return JsonResponse({'MASSAGE':'INVALID_USER'}, status = 401)
+                return JsonResponse({'MESSAGE':'INVALID_USER'}, status = 401)
             
             if not User.objects.get(email = email).password == password :
-                return JsonResponse({'MASSAGE':'INVALID_USER'}, status = 401)
+                return JsonResponse({'MESSAGE':'INVALID_USER'}, status = 401)
             
-            return JsonResponse({'MASSAGE':'SUCCESS'}, status = 200)
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status = 200)
 
         except KeyError :
             return JsonResponse({'MESSAGE':"KEY_ERROR"}, status = 400)
         
         except JSONDecodeError :
             return JsonResponse({'MESSAGE':'JSON_DECODE_ERROR'}, status=400)
+        
+        except User.DoesNotExist :
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status = 401)
