12d5ab86259bb9c670310c171ff2124d70810636
이정아 <junga@ijeong-aui-MacBookPro.local>
2021-09-28 15:46:45 +0900

    models 1차 수정

--- students/junga/westargram/users/models.py
+++ students/junga/westargram/users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=20)
-    user_email = models.EmailField(max_length=100)
-    user_pw = models.CharField(max_length=100)
-    user_tel = models.IntegerField()
-    user_info = models.TextField(max_length=1000)
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    other_information = models.TextField(max_length=1000, null=True)
 
     class Meta:
         db_table = 'users'
