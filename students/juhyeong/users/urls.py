751d270aa33a535bd83c84b64dd8dad40f6e23b6
jh <bndgc@naver.com>
2021-09-29 17:49:53 +0900

    [Add] url 추가

--- students/juhyeong/users/urls.py
+++ students/juhyeong/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from users.views import SignUpsView
+from users.views import SignUpsView, LoginsView
 
 urlpatterns = [
     path('/signup', SignUpsView.as_view()),
+    path('/login', LoginsView.as_view()),
 ]
