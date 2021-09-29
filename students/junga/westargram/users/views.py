3a2925f67a0d7f95c6eca9ba9471085fd68c09b3
이정아 <junga@ijeong-aui-MacBookPro.local>
2021-09-29 19:57:47 +0900

    Add: create SignUpView

--- students/junga/westargram/users/views.py
+++ students/junga/westargram/users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.http    import JsonResponse
+from django.views   import View
+from users.models   import User
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message' : 'ERROR_EMAIL_ALREADY_EXIST'}, status=400)
+
+            if (data['email'] == '') or (data['password'] == ''):
+                return JsonResponse({'message' : 'KEY_ERROR'}, status=400) 
+           
+            if re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", data["email"]) == None:
+                return JsonResponse({"message": "ERROR_EMAIL_NEED_@AND."}, status=400)
+            
+            if re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", data["password"]) == None:
+                return JsonResponse({"message": "ERROR_REQUIRE_8_LETTER,NUMBER,SPECIAL_SYMBOLS)"}, status=400)
+            
+            User.objects.create(
+                name = data['name'],
+                email = data['email'],
+                password = data['password'],
+                phone_number = data['phone_number'],
+                other_information = data.get('other_informaion')
+            )
+            return JsonResponse({'message' : 'SUCCESS'} , status = 201)
+        
+        except Exception:
+            return JsonResponse({'message' : 'FALSE'}, status=400)
