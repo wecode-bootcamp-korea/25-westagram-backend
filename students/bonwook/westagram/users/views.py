28a91b08ef2a7def0e15bc909c7ba863c012c780
qhsdnr0 <qhsdnr0@gmail.com>
2021-09-30 13:16:54 +0900

    Second Modify: LoginView

--- students/bonwook/westagram/users/views.py
+++ students/bonwook/westagram/users/views.py
@@ -11,7 +11,7 @@ class SignupView(View):
         try:
             data = json.loads(request.body)
             
-            if 'password' not in data or 'email' not in data:
+            if not ('password' in data and 'email' in data):
                 return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
             if not validate_email(data['email']):
@@ -20,16 +20,16 @@ class SignupView(View):
             if not validate_password(data['password']):
                 return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
                     
-            if not User.objects.filter(email=data['email']):
-                user = User.objects.create(
+            if User.objects.filter(email=data['email']).exists():                
+                return JsonResponse({'message' : 'DUPLICATED_EMAIL'}, status=400)
+
+            user = User.objects.create(
                     name            = data['name'],
                     email           = data['email'],
                     password        = data['password'],
                     phone_number    = data['phone_number'],
                     date_of_birth   = data.get('date_of_birth'),
                 )
-            else:
-                return JsonResponse({'message' : 'DUPLICATED_EMAIL'}, status=400)
 
             return JsonResponse({'message' : 'CREATED'}, status = 201)
 
@@ -41,7 +41,7 @@ class LoginView(View):
         try:
             data = json.loads(request.body)
 
-            if 'email' not in data or 'password' not in data:
+            if not ('email' in data and 'password' in data):
                 return JsonResponse({"message": "KEY_ERROR"}, status = 400)
             
             if not User.objects.filter(email=data['email']):
@@ -50,8 +50,7 @@ class LoginView(View):
             if User.objects.get(email=data['email']).password != data['password']:
                 return JsonResponse({"message": "INVALID_USER"}, status = 401)
 
-            else:
-                return JsonResponse({"message": "SUCCESS"}, status = 200)
+            return JsonResponse({"message": "SUCCESS"}, status = 200)
 
         except Exception as e:
             return JsonResponse({'message' : e}, status = 400)            
