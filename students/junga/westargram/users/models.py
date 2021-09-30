679bc49b473ed4d7c8bad41979c4dbf87a5303a2
이정아 <junga@ijeong-aui-MacBookPro.local>
2021-09-30 17:20:23 +0900

    views.py, urls.py 수정

--- students/junga/westargram/users/models.py
+++ students/junga/westargram/users/models.py
@@ -6,6 +6,9 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
     other_information = models.TextField(max_length=1000, null=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
--- students/junga/westargram/users/views.py
+++ students/junga/westargram/users/views.py
@@ -1,8 +1,8 @@
-import json
-import re
+import json, re
 
 from django.http    import JsonResponse
 from django.views   import View
+
 from users.models   import User
 
 class SignUpView(View):
@@ -16,11 +16,11 @@ class SignUpView(View):
             if (data['email'] == '') or (data['password'] == ''):
                 return JsonResponse({'message' : 'KEY_ERROR'}, status=400) 
            
-            if re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", data["email"]) == None:
-                return JsonResponse({"message": "ERROR_EMAIL_NEED_@AND."}, status=400)
+            if not re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", data["email"]):
+                return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
             
-            if re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", data["password"]) == None:
-                return JsonResponse({"message": "ERROR_REQUIRE_8_LETTER,NUMBER,SPECIAL_SYMBOLS)"}, status=400)
+            if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", data["password"]):
+                return JsonResponse({"message": "INVALID_PASSWORD)"}, status=400)
             
             User.objects.create(
                 name = data['name'],
@@ -32,4 +32,4 @@ class SignUpView(View):
             return JsonResponse({'message' : 'SUCCESS'} , status = 201)
         
         except Exception:
-            return JsonResponse({'message' : 'FALSE'}, status=400)
+            return JsonResponse({'message' : 'FAILED'}, status=400)
