99014202e921a05ebe32f30b380539f4332cd3a6
jh <bndgc@naver.com>
2021-09-30 09:23:00 +0900

    [Modified] Key Error message 변경

--- students/juhyeong/users/views.py
+++ students/juhyeong/users/views.py
@@ -12,10 +12,10 @@ class SignUpsView(View):
         user = User.objects
 
         if 'email' not in data:
-            return JsonResponse({"message": "KEY_ERROR" + ": email"}, status = 400)
+            return JsonResponse({"message": "email_KEY_ERROR"}, status = 400)
 
         if 'password' not in data:
-            return JsonResponse({"message": "KEY_ERROR"+":password"}, status = 400)
+            return JsonResponse({"message": "password_KEY_ERROR"}, status = 400)
 
         if user.filter(email=data['email']).exists():
             return JsonResponse({"message": "이메일이 이미 등록되었습니다"}, status = 401)
@@ -44,10 +44,10 @@ class LoginsView(View):
         user = User.objects
 
         if 'email' not in data:
-            return JsonResponse({"message": "KEY_ERROR" + " : email"}, status = 400)
+            return JsonResponse({"message": "email_KEY_ERROR"}, status = 400)
 
         if 'password' not in data:
-            return JsonResponse({"message": "KEY_ERROR"+" : password"}, status = 400)
+            return JsonResponse({"message": "password_KEY_ERROR"}, status = 400)
 
         if not user.filter(email=data['email']).filter(password = data['password']).exists():
             return JsonResponse({"message": "INVALID_USER"}, status = 401)
