32a3c25a8ea422ca51bc4a0293ce5edbb5304e5a
이정아 <junga@ijeong-aui-MacBookPro.local>
2021-09-30 17:26:03 +0900

    models.py 수정

--- students/junga/westargram/users/models.py
+++ students/junga/westargram/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=20)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
     other_information = models.TextField(max_length=1000, null=True)
