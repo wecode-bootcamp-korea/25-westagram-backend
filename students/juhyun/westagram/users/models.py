ca6c9eab5c7159da3ad5f79facfa0c905c34885d
kjhabc2002 <juhju2022@gmail.com>
2021-10-03 13:52:31 +0900

    Add : SignUp View complete & modeling fixed

--- students/juhyun/westagram/users/models.py
+++ students/juhyun/westagram/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    email    = models.EmailField(max_length=120)
-    password = models.CharField(max_length=300)
-    username     = models.CharField(max_length=50, null=True)
+    email           = models.EmailField(max_length=120)
+    password        = models.CharField(max_length=300)
+    username        = models.CharField(max_length=50, null=True)
     phone_number    = models.CharField(max_length=50, null=True)
-    created_at    = models.DateTimeField(auto_now_add=True)
-    updated_at    = models.DateTimeField(auto_now=True)
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
     
     
     class Meta:
--- students/juhyun/westagram/users/views.py
+++ students/juhyun/westagram/users/views.py
@@ -1,3 +1,49 @@
+import re
+import jwt
+import json
+import bcrypt
 from django.shortcuts import render
 
-# Create your views here.
+from django.views     import View
+from django.db.models import Q
+from django.http      import JsonResponse
+
+from json.decoder import JSONDecodeError
+from django.http      import JsonResponse
+from .models    import User
+from my_settings import SECRET_KEY, ALGORITHM
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data=json.loads(request.body)
+            
+            email=data['email']
+            password=data['password']
+
+            if User.objects.filter(email=email).exists():
+                    return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=400)
+            
+            
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+                return JsonResponse({'message':'EMAIL_VALIDATION_ERROR'}, status=400)
+            if not re.match('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$', password):
+    	        return JsonResponse({'message':'PASSWORD_VALIDATION_ERROR'}, status=400)
+       
+            
+            User.objects.create(
+                email         = email,
+                phone_number  = data['phone_number'],
+                username      = data['username'],
+                password      = password
+            )
+            return JsonResponse({'message':'SUCCESS'}, status=201)
+        
+        except JSONDecodeError:
+                 return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        
+        
+
+        
