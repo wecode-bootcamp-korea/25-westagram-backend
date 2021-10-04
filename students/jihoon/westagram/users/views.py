6225b97c9e7a506a3aea156acbb4b8650054d448
JiHoon KIM <jihoonkim@JiHoonui-MacBookAir.local>
2021-10-04 20:33:29 +0900

    project brypt-jwt

--- students/jihoon/westagram/users/views.py
+++ students/jihoon/westagram/users/views.py
@@ -1,5 +1,8 @@
-import json
-import re
+
+import  json
+import  re
+import  bcrypt
+import  jwt
 
 from django.http        import JsonResponse
 from django.views       import View
@@ -11,7 +14,7 @@ class SignUpView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            
+
             name        = data['name']
             email       = data['email']
             password    = data['password']
@@ -30,6 +33,9 @@ class SignUpView(View):
             if User.objects.filter(email=email).exists():
                 return JsonResponse({'message':'EMAIL_ALREADY_EXISTS'}, status=400)
 
+            hashed_password =   bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            decoded_password =  hashed_password.decode('utf-8')
+
             User.objects.create(
                 name        =  name,
                 email       =  email,
@@ -39,7 +45,7 @@ class SignUpView(View):
             )
 
             return JsonResponse({'MESSAGE':'SUCCESS'}, status =201)
-        
+
         except KeyError:
             return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400)
 
@@ -52,11 +58,13 @@ class SignInView(View):
                 return JsonResponse({'message': 'UKNOWN_USER'}, status=400)
 
             user = User.objects.get(email=data['email'])
-
-            if user.password != data['password']:
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password != data['password']:
                 return JsonResponse({'message': 'INVAILD_USERS'}, status=401)
 
+            access_token =  jwt.encode({'id' : user.id}, SECRET_KEY, algorithm=ALGORITHM)
             return JsonResponse({"message": "SUCCESS"}, status=200)
 
         except KeyError:
             return JsonResponse({'message': "KEY_ERROR"}, status=400)
+
+            
